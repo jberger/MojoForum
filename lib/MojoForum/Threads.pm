05cdee09076f74a5056ca2df2b0462888b6f487f
Joel Berger <joel.a.berger@gmail.com>
2014-03-15 19:14:26 -0500

    make a toplevel page which shows all of the threads

--- lib/MojoForum/Threads.pm
+++ lib/MojoForum/Threads.pm
@@ -4,6 +4,30 @@ use Mojo::Base 'Mojolicious::Controller';
 
 sub toplevel {
   my $self = shift;
+  $self->render_later;
+  my $delay = Mojo::IOLoop->delay(
+    sub {
+      $self->threads->all(shift->begin)
+    },
+    sub {
+      my ($delay, $err, $threads) = @_;
+      die $err if $err;
+      foreach my $thread (@$threads) {
+        my $end = $delay->begin(0);
+        $thread->creator(sub{
+          my ($thread, $err, $creator) = @_;
+          die $err if $err;
+          $end->([$thread, $creator]);
+        });
+      }
+    },
+    sub {
+      my ($delay, @threads) = @_;
+      $self->stash(threads => \@threads);
+      $self->render; #('threads/toplevel');
+    },
+  );
+  $delay->on(error => sub { $self->render($_[-1]) });
 }
 
 1;
